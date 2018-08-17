d5d590312323f865227e1a56af0e9f2e7ac63e1f
ZZZZeta <fastmuvie@gmail.com>
2018-08-17 19:14:26 +0300

    #2: added employee deleting

--- demo/src/main/js/app.js
+++ demo/src/main/js/app.js
@@ -190,18 +190,30 @@ class EmployeeList extends React.Component{
         )
     }
 
-class Employee extends React.Component{
+    class Employee extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.handleDelete = this.handleDelete.bind(this);
+    }
+
+    handleDelete() {
+        this.props.onDelete(this.props.employee);
+    }
+
     render() {
         return (
             <tr>
                 <td>{this.props.employee.firstName}</td>
                 <td>{this.props.employee.lastName}</td>
                 <td>{this.props.employee.description}</td>
+                <td>
+                    <button onClick={this.handleDelete}>Delete</button>
+                </td>
             </tr>
         )
     }
 }
-
 ReactDOM.render(
     <App />,
     document.getElementById('react')
