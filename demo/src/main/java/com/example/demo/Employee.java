6a2f156b93b96f34af98c7b15f0100f685f8db50
ZZZZeta <fastmuvie@gmail.com>
2018-08-25 17:22:33 +0300

    #3: err fixed: import java Version library

--- demo/src/main/java/com/example/demo/Employee.java
+++ demo/src/main/java/com/example/demo/Employee.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistance.Version;
 
 import lombok.Data;
 
